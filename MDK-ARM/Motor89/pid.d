5bd3b656434281051258b06e61cce9e1371f59ab
WangYH <564286755@qq.com>
2023-05-02 15:40:20 +0800

    Add gitignore

--- MDK-ARM/Motor89/pid.d
+++
@@ -1,3 +0,0 @@
-motor89/pid.o: ..\user\pid\pid.c ..\user\pid\pid.h \
-  ..\user\application\struct_typedef.h \
-  C:\download\ARM\ARMCLANG\Bin\..\include\math.h
--- MDK-ARM/Motor89/startup_stm32f407xx.d
+++
@@ -1,1 +0,0 @@
-motor89\startup_stm32f407xx.o: startup_stm32f407xx.s
